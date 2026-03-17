f15919b1bb9f02ef4e5289a5fec54d403534b0e6
Sathmi0305 <160257306+Sathmi0305@users.noreply.github.com>
2026-03-17 23:37:34 +0530

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker
 import os
  
 DATABASE_URL = os.getenv('DATABASE_URL', '')
